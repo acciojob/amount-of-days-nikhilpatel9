4ac20ecb387be55dfbdfcd3ec33a93f9afb53874
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-05-07 15:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-function daysOfYear(params) {
-	if( (params%4===0 and params%100!==0)|| params%400===0) return 366;
-	return 365
+function daysOfYear(year) {
+	if( (year%4===0 && year%100!==0)|| year%400===0) return 366;
+	else return 365;
 }
